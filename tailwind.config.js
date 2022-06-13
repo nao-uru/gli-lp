818cdfbc58194ffb2dc97052268270480ad32e26
nao <nao.uru0419@gmail.com>
2022-06-13 19:00:02 +0900

    Header~main base

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,4 +14,15 @@ module.exports = {
     './dist/**/*.{js,jsx,ts,tsx,vue,html}',
     "./src/**/*.{html,js}",
   ],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      nevy: '#1E2782',
+      skyBlue: '#E6F0FA',
+      gray:'#6E6E6E',
+      black:'#000000',
+      teal:'#45B4C8',
+      yellow:'#FEFE8B',
+    }
+  }
 }
